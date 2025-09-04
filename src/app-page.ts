87767e90c500300ee96d5035363b36b7d932dcc4
Chen Peleg <chenpeleg@gmail.com>
2025-09-04 23:28:41 +0300

    changed the app page

--- src/app-page.ts
+++ src/app-page.ts
@@ -2,23 +2,6 @@ import {BaseElement} from './_core/elements/base-element.ts';
 
 import vanillaLogo from './assets/images/vanilla-flower.png';
 
-export class SimpleButton extends BaseElement {
-    connectedCallback(): void {
-        super.connectedCallback();
-        this.$<HTMLButtonElement>('button').addEventListener('click', () => this.actionCallback({clicked: true}));
-    }
-    renderTemplate() {
-        // language=HTML
-        this.shadowRoot!.innerHTML = `
-            <button class="   px-4 py-2  text-black bg-slate-200 hover:border-[#646cff] border-2 border-transparent rounded cursor-pointer focus:border-black     transition duration-200">
-                <slot></slot>
-            </button>
-        `;
-    }
-}
-
-customElements.define('simple-button', SimpleButton);
-
 
 class AppPage extends BaseElement {
     private state = {
@@ -27,10 +10,7 @@ class AppPage extends BaseElement {
 
     connectedCallback() {
         super.connectedCallback();
-        this.$<SimpleButton>('simple-button').actionCallback = () => {
-            this.state.clicks++;
-            this.update();
-        }
+
     }
 
     renderTemplate() {
@@ -42,12 +22,14 @@ class AppPage extends BaseElement {
                     <h1 class="text-6xl font-bold text-gray-800 w-full text-center">
                         Vanilla Elements
                     </h1>
-                    <simple-button>
-                        Count is <span id="count-text"> 0 </span>
-                    </simple-button>
+                    Count is <span id="count-text"> 0 </span>
                     <p>
-                        Using  <a class="underline text-blue-500" href="https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements">Custom elements</a> and
-                        <a class="underline text-blue-500" href="https://tailwindcss.com/">TailWind</a>. <a class="underline text-blue-500" href="https://vite.dev/">Vite</a> and 
+                        Using <a class="underline text-blue-500"
+                                 href="https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements">Custom
+                        elements</a> and
+                        <a class="underline text-blue-500" href="https://tailwindcss.com/">TailWind</a>. <a class="underline text-blue-500"
+                                                                                                            href="https://vite.dev/">Vite</a>
+                        and
                         <a class="underline text-blue-500" href="typecriptlang.org/">TypeScript</a> for development.
                     </p>
                 </div>
