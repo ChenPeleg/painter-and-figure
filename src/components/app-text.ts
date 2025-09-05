4182c242a77e50e0a6aebbd0139b1b189fe2674f
Chen Peleg <chenpeleg@gmail.com>
2025-09-05 16:47:45 +0300

    changed things

--- src/components/app-text.ts
+++ src/components/app-text.ts
@@ -3,6 +3,11 @@ import {BookService} from '../services/Book.service.ts';
 
 
 class AppText extends BaseElement {
+    static bigTextShadow = `
+        text-shadow: 2px 2px 4px rgba(0, 0
+, 0, 0.3);
+    `;
+
     static get observedAttributes() {
         return ['page-number'];
     }
@@ -13,7 +18,7 @@ class AppText extends BaseElement {
         this.shadowRoot!.innerHTML = `
             <div class=" flex flex-row justify-center w-full   ">
                 <div class="max-w-screen max-h-screen p-5 text-3xl h-screen  lg:text-3xl leading-11 overflow-y-scroll ">
-                    <p class="flex flex-col gap-6 min-h-fit">
+                    <p style="${AppText.bigTextShadow}" class="flex flex-col gap-6 min-h-fit  ">
                         ${this.getText()}
                         <span id="bottom-padding" class="h-32 w-10 "></span>
                     </p>
@@ -25,12 +30,14 @@ class AppText extends BaseElement {
     protected update() {
 
         const p = this.$<HTMLImageElement>('p');
-        if (!p) return;
+        if (!p) {
+            return;
+        }
         const text = this.getText();
         if (text.length < 100) {
             p.innerText = text;
 
-        } else  {
+        } else {
             this.renderTemplate()
         }
 
