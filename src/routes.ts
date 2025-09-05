a1b92500ca22932905c66cf74046ca435e30df33
Chen Peleg <chenpeleg@gmail.com>
2025-09-05 11:06:04 +0300

    changed images

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import type {RouteObject} from './_core/router/router.ts';
 
 
-export const routes : RouteObject[] = [
-    {
-        path : '/page/:page',
-        index: true,
-        element: ( ) => `<app-page></app-page>`
-    }
+export const routes: RouteObject[] = [{
+    path: '/page/:page',
+    index: true,
+    element: () => `<app-page></app-page>`
+}, {
+    path: '*',
+    element: () => `<router-redirect to="/page/1"></router-redirect>`
+}
+
 
 ]
