2f7c4dd186989ac3209b54170780fa3cc29033e9
Chen Peleg <chenpeleg@gmail.com>
2025-09-05 17:13:16 +0300

    changed things

--- src/components/app-text.ts
+++ src/components/app-text.ts
@@ -1,25 +1,64 @@
 import {BaseElement} from '../_core/elements/base-element.ts';
 import {BookService} from '../services/Book.service.ts';
+import {globalStyleSheet} from '../_core/tailwind-style-sheet.ts';
 
 
 class AppText extends BaseElement {
+    static color = `rgba(255, 255, 255, 1)`
+    static markClass =      `mark {
+  margin: 0 -0.4em;
+  padding: 0.1em 0.4em;
+  border-radius: 0.8em 0.3em;
+  background: transparent;
+  background-image: linear-gradient(
+    to right,
+    rgba(255, 225, 0, 0.1),
+    rgba(255, 225, 0, 0.7) 4%,
+    rgba(255, 225, 0, 0.3)
+  );
+  -webkit-box-decoration-break: clone;
+  box-decoration-break: clone;
+}`
     static bigTextShadow = `
-        text-shadow: 2px 2px 4px rgba(0, 0
-, 0, 0.3);
+        text-shadow: 
+            0 0px 1px ${AppText.color},
+            0 2px 1px ${AppText.color},
+            2px 0px 1px ${AppText.color},
+            2px 2px 1px ${AppText.color}, 
+            0 0px 4px ${AppText.color},
+            0 2px 4px ${AppText.color},
+            2px 0px 4px ${AppText.color},
+            2px 2px 4px ${AppText.color}, 
+               0 0px 6px ${AppText.color},
+            0 2px 6px ${AppText.color},
+            2px 0px 6px ${AppText.color},
+            2px 2px 6px ${AppText.color}, 
+                  0 0px 6px ${AppText.color},
+            0 2px 6px ${AppText.color},
+            2px 0px 6px ${AppText.color},
+            2px 2px 6px ${AppText.color}, 
+            1px 1px 3px rgba(255, 255, 255, 1);
     `;
 
     static get observedAttributes() {
         return ['page-number'];
     }
+    connectedCallback() {
+        super.connectedCallback();
+        const markClassStyleSheet = new CSSStyleSheet();
+        markClassStyleSheet.replaceSync(AppText.markClass);
+        (this.shadowRoot as ShadowRoot).adoptedStyleSheets = [globalStyleSheet, markClassStyleSheet];
+    }
 
     renderTemplate() {
+
 
         // language=HTML
         this.shadowRoot!.innerHTML = `
             <div class=" flex flex-row justify-center w-full   ">
                 <div class="max-w-screen max-h-screen p-5 text-3xl h-screen  lg:text-3xl leading-11 overflow-y-scroll ">
-                    <p style="${AppText.bigTextShadow}" class="flex flex-col gap-6 min-h-fit  ">
-                        ${this.getText()}
+                    <p   class="flex flex-col gap-6 min-h-fit   ">
+                      <span class="mark" id="text"> ${this.getText()} </span> 
                         <span id="bottom-padding" class="h-32 w-10 "></span>
                     </p>
                     </p>
@@ -29,13 +68,13 @@ class AppText extends BaseElement {
 
     protected update() {
 
-        const p = this.$<HTMLImageElement>('p');
-        if (!p) {
+        const spanText = this.$<HTMLSpanElement>('#text');
+        if (!spanText) {
             return;
         }
         const text = this.getText();
         if (text.length < 100) {
-            p.innerText = text;
+            spanText.innerText = text;
 
         } else {
             this.renderTemplate()
@@ -45,9 +84,9 @@ class AppText extends BaseElement {
 
     private getText() {
         const pageNumber = this.getAttribute('page-number') || '1';
-
         const bookService = this.servicesProvider.getService(BookService)
-        return bookService.getPageContent(+pageNumber)
+        const raw =  bookService.getPageContent(+pageNumber)
+        return raw.split('/n/n').map(t=>`<mark>${t}</mark>`).join('<br/><br/>')
     }
 
 }
