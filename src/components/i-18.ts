908d13658803ca16646fb53916d9295aa344a8ac
Chen Peleg <chenpeleg@gmail.com>
2025-09-06 18:04:57 +0300

    more styles

--- src/components/i-18.ts
+++ src/components/i-18.ts
@@ -2,16 +2,16 @@ import {BaseElement} from '../_core/elements/base-element.ts';
 
 
 class I18 extends BaseElement {
-
-    renderTemplate() {
-        this.shadowRoot!.innerHTML = `
-            <span>
-             <slot></slot>
-            </span>
-        `;
+    get observedAttributes () {
+        return ['t'];
     }
 
 
+    renderTemplate() {
+        this.shadowRoot!.innerHTML = `<span id="translated-text">
+             <slot></slot>
+            </span>`;
+    }
 }
 
 customElements.define('i-18', I18);
