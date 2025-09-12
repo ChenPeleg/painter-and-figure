8edd1ce2f25d01e3b09f0eedb812da5634999b7c
Chen Peleg <chenpeleg@gmail.com>
2025-09-12 16:24:40 +0300

    changed image url

--- src/services/Image.service.ts
+++ src/services/Image.service.ts
@@ -1,20 +1,20 @@
 import {ServicesResolver} from '../_global/provider/ServiceResolverClass.ts';
 import {AbstractBaseService} from '../_global/provider/AbstractBaseService.ts';
 
-import image1 from '../assets/sketch/image1.jpeg';
-import image2 from '../assets/sketch/image2.jpeg';
-import image3 from '../assets/sketch/image3.jpeg';
-import image4 from '../assets/sketch/image4.jpeg';
-import image5 from '../assets/sketch/image5.jpeg';
-import image6 from '../assets/sketch/image6.jpeg';
-import image7 from '../assets/sketch/image7.jpeg';
-import image8 from '../assets/sketch/image8.jpeg';
-import image9 from '../assets/sketch/image9.jpeg';
-import image10 from '../assets/sketch/image10.jpeg';
-import image11 from '../assets/sketch/image11.jpeg';
-import image12 from '../assets/sketch/image12.jpeg';
-import image13 from '../assets/sketch/image13.jpeg';
-import image14 from '../assets/sketch/image14.jpeg';
+import image1 from '../../public/sketch/image1.jpeg';
+import image2 from '../../public/sketch/image2.jpeg';
+import image3 from '../../public/sketch/image3.jpeg';
+import image4 from '../../public/sketch/image4.jpeg';
+import image5 from '../../public/sketch/image5.jpeg';
+import image6 from '../../public/sketch/image6.jpeg';
+import image7 from '../../public/sketch/image7.jpeg';
+import image8 from '../../public/sketch/image8.jpeg';
+import image9 from '../../public/sketch/image9.jpeg';
+import image10 from '../../public/sketch/image10.jpeg';
+import image11 from '../../public/sketch/image11.jpeg';
+import image12 from '../../public/sketch/image12.jpeg';
+import image13 from '../../public/sketch/image13.jpeg';
+import image14 from '../../public/sketch/image14.jpeg';
 
 export class ImageService extends AbstractBaseService {
     static allImages: Record<number, string> = {
@@ -33,9 +33,12 @@ export class ImageService extends AbstractBaseService {
         13: image13,
         14: image14
     };
+    private buildImageUrl(imageNumber: number): string {
+        return  `/sketch/image${imageNumber}.jpeg`;
+    }
 
     getImageUrl(imageNumber: number): string {
-        return ImageService.allImages[imageNumber] || '';
+        return this.buildImageUrl(imageNumber);
     }
 
     constructor(provider: ServicesResolver) {
