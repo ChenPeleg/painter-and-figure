24d54fbe6d909793363905d5658417564ab94ed5
Chen Peleg <chenpeleg@gmail.com>
2025-09-12 12:05:10 +0300

    added dark mode

---
+++ src/models/customContent.ts
@@ -0,0 +1,8 @@
+import {Enum} from './Enum.ts';
+
+export class CustomContent extends  Enum
+{
+    static AboutTheProject = new CustomContent('AboutTheProject');
+    static ContactUs = new CustomContent('ContactUs')
+
+}
