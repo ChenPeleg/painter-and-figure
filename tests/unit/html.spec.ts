7a0d769e596b7714b6389e9fe56376506cf06911
Chen Peleg <chenpeleg@gmail.com>
2025-09-14 08:23:23 +0300

    added path

--- tests/unit/html.spec.ts
+++ tests/unit/html.spec.ts
@@ -1,10 +1,15 @@
 import {expect, test} from '@playwright/test';
+import  {readFileSync} from 'fs';
+import {resolve} from 'path';
 
-test.describe('Store Reducer', () => {
+test.describe('Html smoke test', () => {
 
-    test('addOne increments count', async ({page}) => {
-        const htmlPath = resolve(__dirname, '../html/index.html');
+    test('Loading Html', async ({page}) => {
+        const __dirname =  resolve();
+        const htmlPath = resolve(__dirname, 'tests/html/index.html');
         const html =  readFileSync(htmlPath, 'utf-8');
         await page.setContent(html);
+        const title = await page.title();
+        expect(title).toBe('Test Application');
     })
 })
