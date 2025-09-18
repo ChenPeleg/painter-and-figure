2bab5eb02dbc8bc0973f6466a980943455caabef
chenpeleg <chenpeleg@gmail.com>
2025-09-18 13:39:24 +0300

    trying to set the correct root

--- tests/vite.testing-config.ts
+++ tests/vite.testing-config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path';
 
 export default defineConfig({
     server : {
@@ -7,11 +8,14 @@ export default defineConfig({
     },
 
     base: '',
-    publicDir: 'public',
+    publicDir: path.resolve(process.cwd(), 'public'),
     plugins: [tailwindcss(),
     ],
     define: {
         'import.meta.env.VITE_BUILD_DATE': JSON.stringify(new Date().toISOString()),
         'import.meta.env.VITE_APP_VERSION': JSON.stringify(process.env.npm_package_version),
     },
+    root: './html/index.html',
+
+
 });
