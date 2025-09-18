d86c9a80341c5d6786bfe1a2dd82cba660c36204
Chen Peleg <chenpeleg@gmail.com>
2025-09-18 15:48:02 +0300

    fixed environment

--- tests/vite.testing-config.ts
+++ tests/vite.testing-config.ts
@@ -1,22 +1,55 @@
 import {defineConfig} from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path';
+import fs from 'node:fs/promises';
 
-export default defineConfig({
-    server : {
-        port : 4000,
-    },
+export default defineConfig(({ command,
+                                 mode
+                             } )=>{
+    const defaultConfig = {   server : {
+            port : 4000,
+            open : './html/index.html',
+        },
 
-    base: '',
-    publicDir: path.resolve(process.cwd(), 'public'),
-    plugins: [tailwindcss(),
-    ],
-    define: {
-        'import.meta.env.VITE_BUILD_DATE': JSON.stringify(new Date().toISOString()),
-        'import.meta.env.VITE_APP_VERSION': JSON.stringify(process.env.npm_package_version),
-    },
-    // הצב את root לתיקייה שמכילה את index.html
-    root: path.resolve(process.cwd(), 'tests/html'),
+        base: '',
+        publicDir: path.resolve(process.cwd(), 'public'),
+        plugins: [tailwindcss(),
+            {
+                name: 'configure-response-headers',
+                apply: "serve", //   docs:
+                //   https://vitejs.dev/guide/api-plugin.html#conditional-application
+                async transformIndexHtml(html) {
+
+
+                    switch (mode) {
+                        case "contrast_dev": {
+                            const content = await fs.readFile(
+                                "./index-contrast.html",
+                                "utf8"
+                            );
+                            return { html: content, [] };
+                        }
+                        case "uncontrast_dev": {
+                            const content = await fs.readFile(
+                                "./index-uncontrast.html",
+                                "utf8"
+                            );
+                            return { html: content, [] };
+                        }
+                    }
+
+                    return html;
+                },
+
+            }
+
+        ],
+        define: {
+            'import.meta.env.VITE_BUILD_DATE': JSON.stringify(new Date().toISOString()),
+            'import.meta.env.VITE_APP_VERSION': JSON.stringify(process.env.npm_package_version),
+        },
+    }
+    return defaultConfig
 
 
 });
