82e1af2a66b5d4bced1ccaa18e66cd05300ed5a3
Chen Peleg <chenpeleg@gmail.com>
2025-09-19 13:53:55 +0300

    only test chrome

--- tests/unit/translation.spec.ts
+++ tests/unit/translation.spec.ts
@@ -6,9 +6,20 @@ test.describe('Translation Spec', () => {
 
     test('App language button switches between languages', async ({page}) => {
         await page.goto('/')
-        await setPageHtml(page, `<div></div><language-button></language-button> </div`);
+
+        await setPageHtml(page, //language=HTML
+            `
+                <div>
+                    <language-button>
+                    </language-button>
+                    <i-18 t="previousPage"></i-18>
+                </div`);
         const button = page.getByTestId('language-button');
         await expect(button).toBeVisible()
+        await page.getByText('en').first().click();
+        await expect(page.getByText('Previous')).toBeVisible()
+        await page.getByText('en').first().click();
+
     })
 
 })
