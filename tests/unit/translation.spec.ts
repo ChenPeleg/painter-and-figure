beba4eb9c432e271c69973da8b64aff44ab6684d
Chen Peleg <chenpeleg@gmail.com>
2025-09-19 04:46:32 +0300

    fixed vite mode name

--- tests/unit/translation.spec.ts
+++ tests/unit/translation.spec.ts
@@ -6,7 +6,7 @@ test.describe('Translation Spec', () => {
 
     test('App language button switches between languages', async ({page}) => {
         await page.goto('/')
-        await setPageHtml(page, `<div></div><language-button></language-button> avasdfgasdfasd</div`);
+        await setPageHtml(page, `<div></div><language-button></language-button> </div`);
         const button = page.getByTestId('language-button');
         await expect(button).toBeVisible()
     })
